d0bbed12317239600a935a886e32c8a561bd6616
mtumilowicz <michal.tumilowicz01@gmail.com>
2017-11-11 01:16:45 +0100

    AbstractFactory final + tests

--- src/main/java/abstractFactory/widget/PlatformDependentWidgetProducer.java
+++ src/main/java/abstractFactory/widget/PlatformDependentWidgetProducer.java
@@ -7,6 +7,8 @@ import abstractFactory.widget.platform.Platform;
 import abstractFactory.widget.platform.mac.factory.MacOSXWidgetFactory;
 import abstractFactory.widget.platform.ms.factory.MsWindowsWidgetFactory;
 import abstractFactory.widget.window.Window;
+import com.google.common.base.Preconditions;
+import com.sun.javafx.PlatformUtil;
 
 /**
  * Created by mtumilowicz on 2017-11-10.
@@ -15,33 +17,33 @@ public class PlatformDependentWidgetProducer {
     
     private static final GUIBuilder builder = new GUIBuilder();
     
-//    public static Window window() {
-//        Window window = null;
-//        if(PlatformUtil.isMac()){
-//            window = window(Platform.MAC);
-//        }
-//        if (PlatformUtil.isWindows()) {
-//            window = window(Platform.MS);
-//        }
-//        
-//        Preconditions.checkState(window == null, "Platform not supported!");
-//        
-//        return window;
-//    }
-//
-//    public static Button button() {
-//        Button button = null;
-//        if(PlatformUtil.isMac()){
-//            button = button(Platform.MAC);
-//        }
-//        if (PlatformUtil.isWindows()) {
-//            button = button(Platform.MS);
-//        }
-//
-//        Preconditions.checkState(button == null, "Platform not supported!");
-//
-//        return button;
-//    }
+    public static Window window() {
+        Window window = null;
+        if(PlatformUtil.isMac()){
+            window = window(Platform.MAC);
+        }
+        if (PlatformUtil.isWindows()) {
+            window = window(Platform.MS);
+        }
+
+        Preconditions.checkState(window != null, "Platform not supported!");
+
+        return window;
+    }
+
+    public static Button button() {
+        Button button = null;
+        if(PlatformUtil.isMac()){
+            button = button(Platform.MAC);
+        }
+        if (PlatformUtil.isWindows()) {
+            button = button(Platform.MS);
+        }
+
+        Preconditions.checkState(button != null, "Platform not supported!");
+
+        return button;
+    }
 
     public static Window window(Platform platform) {
         return builder.buildWindow(getPlatformDesignedFactory(platform));
--- src/test/java/abstractFactory/PlatformDependentWidgetProducerTest.java
+++ src/test/java/abstractFactory/PlatformDependentWidgetProducerTest.java
@@ -8,14 +8,27 @@ import abstractFactory.widget.platform.mac.window.MacOSXWindow;
 import abstractFactory.widget.platform.ms.button.MSButton;
 import abstractFactory.widget.platform.ms.window.MSWindow;
 import abstractFactory.widget.window.Window;
+import com.sun.javafx.PlatformUtil;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by mtumilowicz on 2017-11-11.
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(PlatformUtil.class )
 public class PlatformDependentWidgetProducerTest {
+
+    @Before
+    public void setUp() {
+        PowerMockito.mockStatic(PlatformUtil.class);
+    }
     
     @Test
     public void MacOSX_Window() {
@@ -39,5 +52,43 @@ public class PlatformDependentWidgetProducerTest {
     public void MS_Button() {
         Button button = PlatformDependentWidgetProducer.button(Platform.MS);
         assertEquals(MSButton.class, button.getClass());
+    }
+
+    @Test
+    public void MSPlatform_window() {
+        PowerMockito.when(PlatformUtil.isWindows()).thenReturn(true);
+        Window window = PlatformDependentWidgetProducer.window();
+        assertEquals(MSWindow.class, window.getClass());
+    }
+
+    @Test
+    public void MSPlatform_button() {
+        PowerMockito.when(PlatformUtil.isWindows()).thenReturn(true);
+        Button button = PlatformDependentWidgetProducer.button(Platform.MS);
+        assertEquals(MSButton.class, button.getClass());
+    }
+
+    @Test
+    public void MacOSXPlatform_window() {
+        PowerMockito.when(PlatformUtil.isMac()).thenReturn(true);
+        Window window = PlatformDependentWidgetProducer.window();
+        assertEquals(MacOSXWindow.class, window.getClass());
+    }
+
+    @Test
+    public void MacOSXPlatform_button() {
+        PowerMockito.when(PlatformUtil.isMac()).thenReturn(true);
+        Button button = PlatformDependentWidgetProducer.button();
+        assertEquals(MacOSXButton.class, button.getClass());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void NotSupportedPlatform_button() {
+        PlatformDependentWidgetProducer.button();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void NotSupportedPlatform_window() {
+        PlatformDependentWidgetProducer.window();
     }
 }
