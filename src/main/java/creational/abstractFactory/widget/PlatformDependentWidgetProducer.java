7e4026365c8ef4f20a87492eb649a8595efc1f63
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-12-11 19:40:50 +0100

    PlatformDependentWidgetProducer.java - removing dependency on static methods PlatformDependentWidgetProducerTest.java - removing dependency on PowerMock

--- src/main/java/creational/abstractFactory/widget/PlatformDependentWidgetProducer.java
+++ src/main/java/creational/abstractFactory/widget/PlatformDependentWidgetProducer.java
@@ -7,69 +7,41 @@ import creational.abstractFactory.widget.platform.Platform;
 import creational.abstractFactory.widget.platform.mac.factory.MacOSXWidgetFactory;
 import creational.abstractFactory.widget.platform.ms.factory.MsWindowsWidgetFactory;
 import creational.abstractFactory.widget.window.Window;
-import com.google.common.base.Preconditions;
-import com.sun.javafx.PlatformUtil;
 
 /**
  * Created by mtumilowicz on 2017-11-10.
  */
 public final class PlatformDependentWidgetProducer {
-    
-    private static final GUIBuilder builder = new GUIBuilder();
 
-    private PlatformDependentWidgetProducer() {
+    private final GUIBuilder builder = new GUIBuilder();
+    private final WidgetFactory widgetFactory;
+
+    public PlatformDependentWidgetProducer(Platform platform) {
+        this.widgetFactory = getPlatformDesignedFactory(platform);
     }
 
-    public static Window window() {
-        Window window = null;
-        if(PlatformUtil.isMac()){
-            window = window(Platform.MAC);
-        }
-        if (PlatformUtil.isWindows()) {
-            window = window(Platform.MS);
-        }
-
-        Preconditions.checkState(window != null, "Platform not supported!");
-
-        return window;
+    public Window window() {
+        return builder.buildWindow(widgetFactory);
     }
 
-    public static Button button() {
-        Button button = null;
-        if(PlatformUtil.isMac()){
-            button = button(Platform.MAC);
-        }
-        if (PlatformUtil.isWindows()) {
-            button = button(Platform.MS);
-        }
-
-        Preconditions.checkState(button != null, "Platform not supported!");
-
-        return button;
+    public Button button() {
+        return builder.buildButton(widgetFactory);
     }
 
-    private static Window window(Platform platform) {
-        return builder.buildWindow(getPlatformDesignedFactory(platform));
-    }
-
-    private static Button button(Platform platform) {
-        return builder.buildButton(getPlatformDesignedFactory(platform));
-    }
-    
     private static WidgetFactory getPlatformDesignedFactory(Platform platform) {
         WidgetFactory widgetFactory;
-        
+
         switch (platform) {
             case MS:
-                widgetFactory  = new MsWindowsWidgetFactory();
+                widgetFactory = new MsWindowsWidgetFactory();
                 break;
             case MAC:
-                widgetFactory  = new MacOSXWidgetFactory();
+                widgetFactory = new MacOSXWidgetFactory();
                 break;
             default:
                 throw new IllegalArgumentException("Platform not supported: " + platform);
         }
-        
+
         return widgetFactory;
     }
 }
--- src/test/java/creational/abstractFactory/PlatformDependentWidgetProducerTest.java
+++ src/test/java/creational/abstractFactory/PlatformDependentWidgetProducerTest.java
@@ -1,69 +1,46 @@
 package creational.abstractFactory;
 
-import com.sun.javafx.PlatformUtil;
 import creational.abstractFactory.widget.PlatformDependentWidgetProducer;
 import creational.abstractFactory.widget.button.Button;
+import creational.abstractFactory.widget.platform.Platform;
 import creational.abstractFactory.widget.platform.mac.button.MacOSXButton;
 import creational.abstractFactory.widget.platform.mac.window.MacOSXWindow;
 import creational.abstractFactory.widget.platform.ms.button.MSButton;
 import creational.abstractFactory.widget.platform.ms.window.MSWindow;
 import creational.abstractFactory.widget.window.Window;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by mtumilowicz on 2017-11-11.
  */
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(PlatformUtil.class)
 public class PlatformDependentWidgetProducerTest {
 
-    @Before
-    public void setUp() {
-        PowerMockito.mockStatic(PlatformUtil.class);
-    }
+    private final PlatformDependentWidgetProducer forMs = new PlatformDependentWidgetProducer(Platform.MS);
+    private final PlatformDependentWidgetProducer forMac = new PlatformDependentWidgetProducer(Platform.MAC);
 
     @Test
     public void MSPlatform_window() {
-        PowerMockito.when(PlatformUtil.isWindows()).thenReturn(true);
-        Window window = PlatformDependentWidgetProducer.window();
+        Window window = forMs.window();
         assertEquals(MSWindow.class, window.getClass());
     }
 
     @Test
     public void MSPlatform_button() {
-        PowerMockito.when(PlatformUtil.isWindows()).thenReturn(true);
-        Button button = PlatformDependentWidgetProducer.button();
+        Button button = forMs.button();
         assertEquals(MSButton.class, button.getClass());
     }
 
     @Test
     public void MacOSXPlatform_window() {
-        PowerMockito.when(PlatformUtil.isMac()).thenReturn(true);
-        Window window = PlatformDependentWidgetProducer.window();
+        Window window = forMac.window();
         assertEquals(MacOSXWindow.class, window.getClass());
     }
 
     @Test
     public void MacOSXPlatform_button() {
-        PowerMockito.when(PlatformUtil.isMac()).thenReturn(true);
-        Button button = PlatformDependentWidgetProducer.button();
+        Button button = forMac.button();
         assertEquals(MacOSXButton.class, button.getClass());
-    }
-
-    @Test(expected = IllegalStateException.class)
-    public void NotSupportedPlatform_button() {
-        PlatformDependentWidgetProducer.button();
-    }
-
-    @Test(expected = IllegalStateException.class)
-    public void NotSupportedPlatform_window() {
-        PlatformDependentWidgetProducer.window();
     }
 }
